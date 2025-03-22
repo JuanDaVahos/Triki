b19f5bf7895cfab05ab1da92b69824aa4b2930b0
JuanDaVahos <juandavidvahosbohorquez4@gmail.com>
2025-03-22 09:50:15 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const casillas = document.querySelectorAll('#casilla');
 casillas.forEach((casilla,index) => {
     casilla.addEventListener('click', (e) => {
         e.preventDefault();
-        e.target.textContent = turno ? 'X' : 'O';
+        e.target.innerHTML = turno ? '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="red"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>' : '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="blue"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /></svg>';
         movimientos[index] = turno;
         casilla.disabled = true;
         if (ganador(0,1,2) || ganador(3,4,5) || ganador(6,7,8) ||
             ganador(0,3,6) || ganador(1,4,7) || ganador(2,5,8) ||
             ganador(0,4,8) || ganador(2,4,6)){
-            document.querySelector('#mensaje').textContent = `Ganador: ${turno?'x':'o'}`;
+            document.querySelector('#mensaje').innerHTML = `Ganador: ${turno?'<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="red"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>':'<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="blue"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /></svg>'}`;
             casillas.forEach(casilla => casilla.disabled = true);
         };
             turno = !turno;
